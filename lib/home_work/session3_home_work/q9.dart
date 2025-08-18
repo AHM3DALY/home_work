e49d56998218a9a2489e29ed55754630428b0df7
AHM3DALY <ahmedila123321@gmail.com>
2025-08-19 01:16:14 +0200

    done

--- lib/home_work/session3_home_work/q9.dart
+++ lib/home_work/session3_home_work/q9.dart
@@ -1,4 +1,17 @@
 /*
-
-
+Exercise 9:
+9. a) Create List> students with two items, each having name and grade.
+b) Print the grade of the second student using index and key.
+c) Add both grades and print the average grade as double.
 */
+void main() {
+  List<Map<String, dynamic>> students = [
+    {'name': 'ahmed', 'grade': 100},
+    {'name': 'ali', 'grade': 75},
+  ];
+  print(students[1]['grade']);
+  int grade1 = students[0]['grade'];
+  int grade2 = students[1]['grade'];
+  double average = (grade1 + grade2) / 2;
+  print(average);
+}

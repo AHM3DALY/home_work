3e725fac9df2c0a552fd4070d018237a48f323a0
AHM3DALY <ahmedila123321@gmail.com>
2025-08-24 15:48:47 +0200

    done

--- lib/home_work/session4_homework/q4.dart
+++ lib/home_work/session4_homework/q4.dart
@@ -4,24 +4,9 @@ Create a Dart program that safely reads a phone number from a map. If the phone 
 print a default message. Then update the phone number and print its length.
  */
 
-// void main() {
-//   Map<String, dynamic?> personalDetail = {
-//     'Name': "Hazem",
-//     'Age': 27,
-//     'Phone': null,
-//   };
-
-//   if (personalDetail['Phone'] == null) {
-//     print('Please enter your phone number');
-//   }
-//   personalDetail['Phone'] = '1234567890';
-//   print("${personalDetail['Phone']}");
-//   print(personalDetail['Phone'].length);
-// }
-
 void main() {
   Map<String, dynamic> personalDetail = {
-    'Name': 'Hazem',
+    'Name': 'ahmed',
     'Age': 27,
     'Phone': null,
   };

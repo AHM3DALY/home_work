aa984f62ded0d93709d041d090166f56b6b92793
AHM3DALY <ahmedila123321@gmail.com>
2025-08-24 17:10:58 +0200

    done

--- lib/home_work/session4_homework/q11.dart
+++ lib/home_work/session4_homework/q11.dart
@@ -4,15 +4,15 @@ Write a Dart program that converts a list of names to a set of unique values. Cr
 counts of occurrences. Compare lengths and print a message if a specific name appears more than once.
  */
 void main() {
-  List<String> names = ['Hazem', 'Ali', 'Sami', 'Ali', 'Hazem'];
+  List<String> names = ['ahmed', 'Ali', 'Sami', 'Ali', 'hossam'];
   Set<String> namesUnique = names.toSet();
-  Map<String, int> countsOfName = {'Hazem': 2, 'Sami': 1, 'Ali': 2};
+  Map<String, int> countsOfName = {'hossam': 2, 'Sami': 1, 'Ali': 2};
   bool namesHigher = names.length > namesUnique.length;
   print(namesHigher);
-  if (countsOfName['Hazem']! > 1) {
-    print('Hazem is duplicated');
+  if (countsOfName['hossam']! > 1) {
+    print('hossam is duplicated');
   } else {
-    print('Hazem is not duplicated');
+    print('hossam is not duplicated');
   }
   ;
   if (countsOfName['Sami']! > 1) {

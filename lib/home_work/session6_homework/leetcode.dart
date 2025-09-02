fc77442c6a2b40793f52f40fd2abe8cba6985c24
AHM3DALY <ahmedila123321@gmail.com>
2025-09-02 21:50:57 +0200

    done

--- lib/home_work/session6_homework/leetcode.dart
+++ lib/home_work/session6_homework/leetcode.dart
@@ -10,22 +10,18 @@ void main() {
   var nums = [2, 7, 11, 15];
   var target = 9;
 
-  var solution = Solution();
-  var result = solution.twoSum(nums, target);
-
+  var result = twoSum(nums, target);
   print(result); // [0, 1]
 }
 
-class Solution {
-  List<int> twoSum(List<int> nums, int target) {
-    Map<int, int> numMap = {};
-    for (int i = 0; i < nums.length; i++) {
-      int complement = target - nums[i];
-      if (numMap.containsKey(complement)) {
-        return [numMap[complement]!, i];
-      }
-      numMap[nums[i]] = i;
+List<int> twoSum(List<int> nums, int target) {
+  Map<int, int> numMap = {};
+  for (int i = 0; i < nums.length; i++) {
+    int complement = target - nums[i];
+    if (numMap.containsKey(complement)) {
+      return [numMap[complement]!, i];
     }
-    return [];
+    numMap[nums[i]] = i;
   }
+  return [];
 }
